8012341276181f0cb994a3e876b062a0af969470
madeleine-b <madeleine-b@users.noreply.github.com>
2017-11-11 16:04:08 -0500

    in progress of doing phone to text pipeline

--- story-app/get_title.js
+++ story-app/get_title.js
@@ -2,39 +2,85 @@ const express = require('express');
 const jquery = require('jquery');
 
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
-//const urlencoded = require('body-parser').urlencoded;
+const urlencoded = require('body-parser').urlencoded;
 
 const app = express();
-/*app.use(urlencoded({extended: false}));
+app.use(urlencoded({extended: false}));
 app.use((request, response, next) => {
   response.type('text/xml');
   next();
-});  */
+});  
 
 var storyTitle = "Story Title";
+var processedSnippets = [];
 
 app.get('/', function (request, response) {
-  res.sendFile(__dirname+'/index.html');
+  response.sendFile(__dirname+'/index.html');
 });
 
 app.post('/voice', (request, response) => {
   const twiml = new VoiceResponse();
-  twiml.say('What is the title of the story?', { voice: 'alice' });
+  twiml.say('What is the title of your story?', { voice: 'alice' });
   const gather = twiml.gather({
     timeout: '5',
     speechTimeout: 'auto',
     input: 'speech',
-    action: '/gettitle'
+    action: '/getTitle'
   });
   response.type('text/xml');
   response.send(twiml.toString());
 });
 
-app.post('/gettitle', (request, response) => {
-  storyTitle = request.SpeechResult;
-  console.log(storyTitle);
-  
-  response.sendFile(__dirname+'/index.html');
+function repeatTitle(twiml) {
+  twiml.say("Sorry, didn't quite catch that.", { voice: 'alice' });
+  twiml.redirect(307, '/voice');
+  return twiml;
+}
+
+app.post('/getTitle', (request, response) => {
+  const twiml = new VoiceResponse();
+  response.type('text/xml');
+  storyTitle = request.body.SpeechResult;
+  var confidence = request.body.Confidence;
+  console.log('Heard the title as: ' + storyTitle + " (with confidence " + confidence.toString());
+  if (confidence < 0.5) { // test this branch still
+    var finalResponse = repeatTitle(twiml);
+    response.send(finalResponse.toString());
+  }
+
+  console.log(twiml.toString())
+  var finalResponse = listenToStory(twiml, storyTitle);
+  response.send(finalResponse.toString());
+  response.redirect(307, '/listenToStory');
+});
+
+function listenToStory(twiml, storyTitle){
+  twiml.say("Whenever you're ready, tell the story called "+storyTitle, { voice: 'alice' });
+  twiml.say
+  twmiml.gather
+  return twiml
+
+}
+
+app.get('/listenToStory', (request, response) => {
+  const twiml = new VoiceResponse();
+  console.log("we are here");
+  console.log(request.body.SpeechResult);
+  processedSnippets += request.body.SpeechResult;
+  if (processedSnippets.length > 10) {
+    twiml.say("Goodbye", {voice: 'alice'});
+    twiml.hangup();
+    response.type('text/xml');
+    response.send(twiml.toString());
+  }
+  const gather = twiml.gather({
+    timeout: '5',
+    speechTimeout: 'auto',
+    input: 'speech',
+    action: '/listenToStory'
+  });
+  response.type('text/xml');
+  response.send(twiml.toString());
 });
 
 app.listen(3000, function() {
