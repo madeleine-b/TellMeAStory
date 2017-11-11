deff1c56e81780ccdc13e50c1c12de87ffb08c5a
madeleine-b <madeleine-b@users.noreply.github.com>
2017-11-11 16:45:42 -0500

    idk nothing works

--- story-app/get_title.js
+++ story-app/get_title.js
@@ -43,45 +43,58 @@ app.post('/getTitle', (request, response) => {
   storyTitle = request.body.SpeechResult;
   var confidence = request.body.Confidence;
   console.log('Heard the title as: ' + storyTitle + " (with confidence " + confidence.toString());
-  if (confidence < 0.5) { // test this branch still
+  if (confidence < 0.70) { // still need to test this branch 
     var finalResponse = repeatTitle(twiml);
     response.send(finalResponse.toString());
   }
-
-  console.log(twiml.toString())
-  var finalResponse = listenToStory(twiml, storyTitle);
-  response.send(finalResponse.toString());
   response.redirect(307, '/listenToStory');
 });
 
-function listenToStory(twiml, storyTitle){
-  twiml.say("Whenever you're ready, tell the story called "+storyTitle, { voice: 'alice' });
-  twiml.say
-  twmiml.gather
-  return twiml
-
-}
-
-app.get('/listenToStory', (request, response) => {
-  const twiml = new VoiceResponse();
+/*function listenToStory(twiml, storyTitle){
   console.log("we are here");
-  console.log(request.body.SpeechResult);
+  twiml.say("Whenever you're ready, tell the story called "+storyTitle, { voice: 'alice' });
   processedSnippets += request.body.SpeechResult;
   if (processedSnippets.length > 10) {
     twiml.say("Goodbye", {voice: 'alice'});
     twiml.hangup();
-    response.type('text/xml');
-    response.send(twiml.toString());
+    return twiml;
   }
   const gather = twiml.gather({
     timeout: '5',
     speechTimeout: 'auto',
-    input: 'speech',
-    action: '/listenToStory'
+    input: 'speech'
+  // continue listening... 
   });
+  return twiml;
+} */
+
+app.get('/listenToStory', (request, response) => {
+  const twiml = new VoiceResponse();
+  twiml.say("Whenever you're ready, tell the story called "+storyTitle, { voice: 'alice' });
+  
+  function gather() {
+    const gatherNode = twiml.gather({
+      timeout: '10',
+      speechTimeout: 'auto',
+      input: 'speech'
+    });
+
+    twiml.redirect('/listenToStory');
+  }
+
+  while (processedSnippets.length < 10) {
+    gather();
+    console.log(request.body.SpeechResult);
+    if (request.body.SpeechResult) {
+      processedSnippets += request.body.SpeechResult;
+    }
+  }
+  twiml.say("Goodbye", {voice: 'alice'});
+  twiml.hangup();
+    
   response.type('text/xml');
   response.send(twiml.toString());
-});
+}); 
 
 app.listen(3000, function() {
     console.log('System app HTTP server running at http://127.0.0.1:3000');
