d8ed305c99d41c4281729f00479abd86dde5d844
madeleine-b <madeleine-b@users.noreply.github.com>
2017-11-11 19:20:39 -0500

    setting up GET endpoint for giving next text fragment and story title

--- story-app/get_title.js
+++ story-app/get_title.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const jquery = require('jquery');
 
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const urlencoded = require('body-parser').urlencoded;
@@ -7,83 +6,94 @@ const urlencoded = require('body-parser').urlencoded;
 const app = express();
 app.use(urlencoded({extended: false}));
 app.use((request, response, next) => {
-  response.type('text/xml');
-  next();
+	response.type('text/xml');
+	next();
 });  
 
 var storyTitle = "Story Title";
 var processedSnippets = [];
+var snippetQueue = ["a", "b", "c"];
 
 app.get('/', function (request, response) {
-  response.sendFile(__dirname+'/index.html');
+	response.sendFile(__dirname+'/index.html');
 });
 
 app.post('/voice', (request, response) => {
-  const twiml = new VoiceResponse();
-  processedSnippets = [];  // clear out any old snippets 
-  twiml.say('What is the title of your story?', { voice: 'alice' });
-  const gather = twiml.gather({
-    timeout: '10',
-    speechTimeout: 'auto',
-    input: 'speech',
-    action: '/getTitle'
-  });
-  response.type('text/xml');
-  response.send(twiml.toString());
+	const twiml = new VoiceResponse();
+	processedSnippets = [];  // clear out any old snippets 
+	snippetQueue = [];
+	twiml.say('What is the title of your story?', { voice: 'alice' });
+	const gather = twiml.gather({
+		timeout: '10',
+		speechTimeout: 'auto',
+		input: 'speech',
+		action: '/getTitle'
+	});
+	response.type('text/xml');
+	response.send(twiml.toString());
 });
 
 app.post('/getTitle', (request, response) => {
-  const twiml = new VoiceResponse();
-  response.type('text/xml');
-  storyTitle = request.body.SpeechResult;
-  var confidence = request.body.Confidence;
-  console.log('Heard the title as: ' + storyTitle + " (with confidence " + confidence.toString() + ")");
-  if (confidence < 0.70) { // still need to test this branch 
-    const twiml_temp = new VoiceResponse();
-    twiml_temp.say("Sorry, didn't quite catch that.", { voice: 'alice' });
-    twiml.redirect(307, '/voice');
-  }
-  twiml.say("Whenever you're ready, tell the story called "+storyTitle, { voice: 'alice' });
-  var finalResponse = listenToStory(twiml, storyTitle);
-  response.send(finalResponse.toString());
+	const twiml = new VoiceResponse();
+	response.type('text/xml');
+	storyTitle = request.body.SpeechResult;
+	var confidence = request.body.Confidence;
+	console.log('Heard the title as: ' + storyTitle + " (with confidence " + confidence.toString() + ")");
+	if (confidence < 0.70) { // still need to test this branch 
+		const twiml_temp = new VoiceResponse();
+		twiml_temp.say("Sorry, didn't quite catch that.", { voice: 'alice' });
+		twiml.redirect(307, '/voice');
+	}
+	twiml.say("Whenever you're ready, tell the story called "+storyTitle, { voice: 'alice' });
+	var finalResponse = listenToStory(twiml, storyTitle);
+	response.send(finalResponse.toString());
+});
+
+app.get('/nextSnippet', (request, response) => {
+	var snippetToSend = snippetQueue[0] ? snippetQueue[0] : '';
+	var jsonSnippets = JSON.stringify({'title' : storyTitle, 'text' : snippetToSend});
+	snippetQueue.splice(0, 1);  // remove first element
+	console.log(snippetQueue);
+	response.setHeader('Content-Type', 'application/json');
+	response.send(jsonSnippets);
 });
 
 function listenToStory(twiml, storyTitle){
-  console.log("Beginning to listen to the story");  
-  const gatherNode = twiml.gather({
-      timeout: '10',
-      speechTimeout: 'auto',
-      input: 'speech', 
-      action: '/continueListening'
-  });
-  
-  return twiml;
+	console.log("Beginning to listen to the story");  
+	const gatherNode = twiml.gather({
+			timeout: '10',
+			speechTimeout: 'auto',
+			input: 'speech', 
+			action: '/continueListening'
+	});
+	
+	return twiml;
 } 
 
 app.post('/continueListening', (request, response) => {
-  const twiml2 = new VoiceResponse();
+	const twiml2 = new VoiceResponse();
 
-  console.log("Story snippet: " + request.body.SpeechResult);
-  processedSnippets.push(request.body.SpeechResult);
-  console.log(processedSnippets);
+	console.log("Story snippet: " + request.body.SpeechResult);
+	processedSnippets.push(request.body.SpeechResult);
+	snippetQueue.push(request.body.SpeechResult);
 
-  if (processedSnippets.length > 10) {
-    twiml2.say("Goodbye", {voice: 'alice'});
-    twiml2.hangup();
-    response.send(twiml2.toString());
-  }
+	if (processedSnippets.length > 10) {
+		twiml2.say("Goodbye", {voice: 'alice'});
+		twiml2.hangup();
+		response.send(twiml2.toString());
+	}
 
-  const gatherNode = twiml2.gather({
-      timeout: '5',
-      speechTimeout: 'auto',
-      input: 'speech',
-      action: '/continueListening'
-    });
+	const gatherNode = twiml2.gather({
+			timeout: '10',
+			speechTimeout: 'auto',
+			input: 'speech',
+			action: '/continueListening'
+		});
 
-  response.type('text/xml');
-  response.send(twiml2.toString());
+	response.type('text/xml');
+	response.send(twiml2.toString());
  }); 
 
 app.listen(3000, function() {
-    console.log('System app HTTP server running at http://127.0.0.1:3000');
+		console.log('System app HTTP server running at http://127.0.0.1:3000');
 });
