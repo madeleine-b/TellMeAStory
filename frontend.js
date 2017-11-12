77f67360e8a67bd42d441adc48676a61af1e92f6
Priscilla Lee <plee3@wellesley.edu>
2017-11-11 22:00:21 -0500

    removed commented lines

--- frontend.js
+++ frontend.js
@@ -128,7 +128,7 @@ function endStory() {
 
 function addText() {
 	$.ajax({
-		'url' : 'http://66f70844.ngrok.io/nextSnippet', //'http://c0cce248.ngrok.io/nextSnippet',
+		'url' : 'http://66570844.ngrok.io/nextSnippet', 
     	'type' : 'GET'}).then(result => {
     		if (!startedStory && result.title != '') {
     			tellTitle(result.title);
